0b43056b0c388bf5e17523048a6ec5cb8657bcb7
Sean Hanlon <sean.a.hanlon@gmail.com>
2023-09-19 22:06:54 +0100

    "Added book class"

---
+++ src/main/java/com/codeclan/Book.java
@@ -0,0 +1,37 @@
+package com.codeclan;
+
+public class Book {
+  private String title;
+  private String author;
+  private String genre;
+
+  public Book() {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public String getGenre() {
+    return genre;
+  }
+
+  public void setGenre(String genre) {
+    this.genre = genre;
+  }
+}
