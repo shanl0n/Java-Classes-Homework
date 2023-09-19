66362b51d230e85ec5b649e7bdabfcc1e9e41a3a
Sean Hanlon <sean.a.hanlon@gmail.com>
2023-09-19 20:12:15 +0100

    "First commit with setup"

---
+++ src/main/java/com/codeclan/Library.java
@@ -0,0 +1,15 @@
+package com.codeclan;
+
+import java.util.ArrayList;
+
+public class Library {
+
+  private ArrayList<String> books;
+
+  public Library(ArrayList<String> books){
+    this.books = books;
+  }
+
+
+
+}
